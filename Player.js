c8efd0251a36d7ed4ff3f6a394ed4ebeaa290917
shreyaseemullick <78734018+shreyaseemullick@users.noreply.github.com>
2021-03-15 23:18:06 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,13 @@
+class Player{
+    constructor(x,y){
+        this.x= x;
+        this.y= y;
+        this.spt= createSprite(this.x, this.y, 50, 50);
+        this.spt.shapeColor= "orange";
+        player.spt.addAnimation("player",playerImage);
+    }
+    move(xdir,ydir){
+        this.spt.x += xdir*grid;
+        this.spt.y += ydir*grid;
+    }
+}
